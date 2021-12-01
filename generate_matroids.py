bc67be78af020c14ac84176ebd94f489180e034f
Alex Valtchanov <avv2@princeton.edu>
2021-11-30 22:02:38 -0500

    minor fix to sampling bases randomly

--- generate_matroids.py
+++ generate_matroids.py
@@ -4,18 +4,17 @@ import math
 from test_algorithm import Matroid
 
 def random_matriod(n, rank, num_bases):
-    assert num_bases <= math.comb(n, rank), 'number of bases must be at most nCr(n, rank)'
+    max_bases = math.comb(n, rank)
+    assert num_bases <= max_bases, 'number of bases must be at most nCr(n, rank)'
     assert rank <= n, 'rank cannot exceed n'
 
-    universe = list(range(n))
-    all_bases = itertools.combinations(universe, rank)
-    random.shuffle(universe)
+    all_bases = itertools.combinations(range(n), rank)
+    bases_indices = random.sample(range(max_bases), num_bases)
 
     # select num_bases randomly
-    indices = set(universe[:num_bases])
     bases = set()
     for i, b in enumerate(all_bases):
-        if i in indices: bases.add(frozenset(b)) 
+        if i in bases_indices: bases.add(frozenset(b)) 
 
     # generate all subsets of all bases, could maybe be made more efficient
     ind_sets = set(frozenset(x) for b in bases for i in range(1, rank+1) for x in itertools.combinations(b, i))
