a856e9704b6273ce6a7c6cc96ebaf9727a3747a2
Alex Valtchanov <avv2@princeton.edu>
2021-11-30 22:08:01 -0500

    sample bases more simply

--- generate_matroids.py
+++ generate_matroids.py
@@ -8,18 +8,13 @@ def random_matriod(n, rank, num_bases):
     assert num_bases <= max_bases, 'number of bases must be at most nCr(n, rank)'
     assert rank <= n, 'rank cannot exceed n'
 
-    all_bases = itertools.combinations(range(n), rank)
-    bases_indices = set(random.sample(range(max_bases), num_bases))
-
     # select num_bases randomly
-    bases = set()
-    for i, b in enumerate(all_bases):
-        if i in bases_indices: bases.add(frozenset(b)) 
+    bases = random.sample(list(itertools.combinations(range(n), rank)), num_bases) 
 
     # generate all subsets of all bases, could maybe be made more efficient
     ind_sets = set(frozenset(x) for b in bases for i in range(1, rank+1) for x in itertools.combinations(b, i))
     return Matroid(n, ind_sets)
 
 if __name__ == '__main__':
-    M = random_matriod(5, 3, 3)
+    M = random_matriod(5, 5, 1)
     print(M)
